9e414467148f879b640234a5ec928427f9004947
hothman <houcemoo@gmail.com>
2020-05-06 16:10:43 +0200

    add filterVars process

--- main/swaat.groovy
+++ main/swaat.groovy
@@ -16,19 +16,24 @@ params.SCRIPTS="/home/houcemeddine/BILIM/SWAAT/scripts"
 // Directory for all outputs 
 params.OUTFOLDER = "/home/houcemeddine/BILIM/testing_SWAAT/swaat_output"
 
+// Path to database HOME
+params.DATABASE="/home/houcemeddine/BILIM/testing_SWAAT/myoutput"
+
 // list of gene name to process (one gene name per line)
 params.GENELIST="../exampleInputs/gene_list.txt"
 
 
 // generate a channel from the gene list and replace "\n"  
 genes = Channel.fromPath("$params.GENELIST").splitText()  { it.replaceAll("\n", "") }
-//genes.subscribe { println "File: ${it} => ${it}" }
+		.ifEmpty { error "Cannot find genes in gene list" }
 
+
+//genes.subscribe { println "File: ${it} => ${it}" }
 // generate the list of missens variants from VCF and Map files
 process generate_swaat_input {
 	publishDir "${params.OUTFOLDER}/$gene", mode:'copy'
 	input:
-		val gene from genes.flatMap()
+		val(gene) from genes.flatMap()
 	output: 
 		file "${gene}_var2prot.csv" into var2aa_report
 		file "${gene}.swaat" into swaat_input
@@ -45,6 +50,75 @@ process generate_swaat_input {
 										 --output ${gene}_var2prot.csv \
 										 --swaat ${gene}.swaat
 	"""
+}
+
+
+process filterVars {
+	echo true
+	/* execution error is ignored if no variant is in the range of the 
+	 exons or, it is not covered by the protein structure. or 
+	 no *not_processed.tsv is generated 
+	*/
+	errorStrategy 'ignore'
+	
+	input: 
+		file variants from swaat_input
+	output: 
+		file("${name}_retained.tsv") into retained_vars
+		file("${name}_not_processed.tsv") into gaps_and_non_processed
+
+	publishDir "${params.OUTFOLDER}/$name", mode:'copy'
+
+	script:
+		name = variants.baseName.replaceFirst(".swaat","")
+	"""
+	#!/usr/bin/env python
+	import glob
+	print("$name")
+	with open("$variants", "r") as input: 
+		lines=input.readlines() 
+	for datafile in glob.glob("${params.DATABASE}/uniprot2PDBmap" + "/*.tsv"):
+		print() 
+		# supposes that the variant file contains only one gene 
+		gene_name_in_vars = lines[0].split()[0]
+		with open(datafile, "r") as mydatafile: 
+			datalines=mydatafile.readlines() 
+			gene_name_in_map=datalines[0].split()[1]
+			if  gene_name_in_map == gene_name_in_vars:
+				covered_residues=[line.split()[1] for line in datalines[2:]]
+				for var in lines: 
+					if (var.split()[2] in covered_residues) and (var.split()[3] != "_") :
+						with open("${name}_retained.tsv", "a+") as processed_var: 
+							processed_var.writelines(var)
+
+					elif var.split()[3] == "_" : 
+						with open("${name}_not_processed.tsv", "a+") as not_processed: 
+							not_processed.writelines(var)
+					else: 
+						with open("${name}_not_processed.tsv", "a+") as not_processed: 
+							not_processed.writelines(var)
+	"""
+
+
+
 
 }
 
+/*
+
+vars = swaat_input.splitText()  { it.replaceAll("\n", "") }
+//vars.println()
+
+process calculateFoldXddG {
+	input: 
+		val variant from vars.flatMap()
+	output: 
+		file "myvar.txt"
+
+
+	"""
+	echo "$variant"  >myvar.txt
+	"""
+	}
+
+	*/
