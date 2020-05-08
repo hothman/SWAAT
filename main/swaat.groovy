a8d7c560281c4dd69ae98bb1cfde17a4790cd08a
hothman <houcemoo@gmail.com>
2020-05-08 17:12:03 +0200

    Add the generateGuidingFile process

--- main/swaat.groovy
+++ main/swaat.groovy
@@ -22,6 +22,12 @@ params.DATABASE="/home/houcemeddine/BILIM/testing_SWAAT/myoutput"
 // list of gene name to process (one gene name per line)
 params.GENELIST="../exampleInputs/gene_list.txt"
 
+// home to script file 
+params.SCRIPTHOME = "/home/houcemeddine/BILIM/SWAAT/scripts/"
+
+// home to PDBs 
+params.PDBFILESPATH = "/home/houcemeddine/BILIM/testing_SWAAT/PDBs" 
+
 
 // generate a channel from the gene list and replace "\n"  
 genes = Channel.fromPath("$params.GENELIST").splitText()  { it.replaceAll("\n", "") }
@@ -59,13 +65,15 @@ process filterVars {
 	 exons or, it is not covered by the protein structure. or 
 	 no *not_processed.tsv is generated 
 	*/
-	errorStrategy 'ignore'
+	//errorStrategy 'ignore'
 	
 	input: 
-		file variants from swaat_input
+		file(variants) from swaat_input
 	output: 
-		file("${name}_retained.tsv") into retained_vars
-		file("${name}_not_processed.tsv") into gaps_and_non_processed
+		file("${name}_retained.tsv") optional true into retained_vars
+		file("${name}_not_processed.tsv") optional true into gaps_and_non_processed
+		//file("${name}_pointer.tsv") optional true into pointerfile
+  		val name into receiver
 
 	publishDir "${params.OUTFOLDER}/$name", mode:'copy'
 
@@ -78,7 +86,6 @@ process filterVars {
 	with open("$variants", "r") as input: 
 		lines=input.readlines() 
 	for datafile in glob.glob("${params.DATABASE}/uniprot2PDBmap" + "/*.tsv"):
-		print() 
 		# supposes that the variant file contains only one gene 
 		gene_name_in_vars = lines[0].split()[0]
 		with open(datafile, "r") as mydatafile: 
@@ -86,6 +93,14 @@ process filterVars {
 			gene_name_in_map=datalines[0].split()[1]
 			if  gene_name_in_map == gene_name_in_vars:
 				covered_residues=[line.split()[1] for line in datalines[2:]]
+				try: 
+					info_line = datalines[0].split()
+					fasta = info_line[1]+".fasta"
+					pdb = info_line[4]
+					with open("${name}_pointer.txt", "w") as pointerfile: 
+						pointerfile.writelines(fasta+"\t"+pdb)				
+				except: 
+					pass
 				for var in lines: 
 					if (var.split()[2] in covered_residues) and (var.split()[3] != "_") :
 						with open("${name}_retained.tsv", "a+") as processed_var: 
@@ -99,26 +114,26 @@ process filterVars {
 							not_processed.writelines(var)
 	"""
 
-
-
-
 }
 
+
 /*
+This process generates the guiding file (which sequence and which PDB for the mutation)
+*/
 
-vars = swaat_input.splitText()  { it.replaceAll("\n", "") }
-//vars.println()
-
-process calculateFoldXddG {
+process generateGuidingFile {
 	input: 
 		val variant from vars.flatMap()
+		val name from receiver
 	output: 
-		file "myvar.txt"
-
+		file "${name}_whichPDB.tsv" into guidingFile
 
 	"""
-	echo "$variant"  >myvar.txt
+	python ${params.SCRIPTHOME}/whichPdb.py --fasta     ${params.DATABASE}/sequences/${name}.fa \
+											--pdbpath  ${params.PDBFILESPATH} --output   ${name}_whichPDB.tsv
 	"""
 	}
 
-	*/
+
+vars = retained_vars.splitText() { it.replaceAll("\n", "") }
+//thepointer = pointerfile.splitText() { it.replaceAll("\n", "") }
